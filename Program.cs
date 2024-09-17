e21f61bd9f0dbddfbf7367ab96e866aba3ef16ba
Srikanth M <srikanth.mangalampally@intelliflo.com>
2024-09-17 18:27:52 +0530

    Email service changes

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
+using Intelliflo.Finance.Service.Helpers;
 using Intelliflo.Finance.Service.Repositories.Contracts;
 using Intelliflo.Finance.Service.Repositories.Services;
+using NETCore.MailKit.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+var mailSettings = new MailSettings();
+builder.Configuration.GetSection("MailSettings").Bind(mailSettings);
+
+// Add MailKit with manually bound configuration
+builder.Services.AddMailKit(config => config.UseMailKit(mailSettings));
 
 // Add Swagger services
 builder.Services.AddEndpointsApiExplorer();
@@ -31,11 +38,8 @@ app.UseSwagger();
 // Enable middleware to serve Swagger UI
 app.UseSwaggerUI();
 // Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
---
+++ Services/EmailService.cs
@@ -0,0 +1,19 @@
+﻿using NETCore.MailKit.Core;
+
+namespace Intelliflo.Finance.Service.Services
+{
+    public class EmailService
+    {
+        private readonly IEmailService _emailService;
+
+        public EmailService(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        {
+            await _emailService.SendAsync(toEmail, subject, body, isHtml: true);
+        }
+    }
+}
